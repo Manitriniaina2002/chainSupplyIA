71dde434e261ea56012e5edeefb6941123666f15
Stellam2220 <mariestellamrivoharilala@gmail.com>
2025-10-08 17:41:24 +0300

    Ajout de VITE_BASE_PATH

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+        outDir: 'dist', // Doit correspondre à l'Output Directory de Vercel
+  },
 })
